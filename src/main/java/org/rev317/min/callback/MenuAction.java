f0b60f1330bf87a7a54a9363166b1d8d89a9fd4a
JKetelaar <jeroen@ketelaar.me>
2017-08-05 00:59:32 +0200

    [BUGFIX] Added method to get index for PkHonor

--- src/main/java/org/rev317/min/callback/MenuAction.java
+++ src/main/java/org/rev317/min/callback/MenuAction.java
@@ -1,5 +1,7 @@
 package org.rev317.min.callback;
 
+import org.parabot.core.Context;
+import org.parabot.core.reflect.RefClass;
 import org.rev317.min.Loader;
 import org.rev317.min.accessors.Client;
 import org.rev317.min.api.events.GameActionEvent;
@@ -24,7 +26,8 @@ public class MenuAction {
     };
     private static int currentOutputIndex = 0;
 
-    public static void intercept(int index) {
+    public static void intercept() {
+        int index = getActionIndex();
         int outputIndex = 0;
 
         Client client = Loader.getClient();
@@ -60,5 +63,58 @@ public class MenuAction {
 
     public static String[][] getOutputs() {
         return outputs;
+    }
+
+    private static int getActionIndex(){
+        RefClass client = new RefClass(Context.getInstance().getClient());
+
+        if (client.getField("TiSK").asInt() != 0) {
+            return -1;
+        }
+        int i = client.getField("gDnF").asInt();
+        if ((client.getField("PjW").asInt() == 1) && (client.getField("PJ").asInt() >= 516) && (client.getField("Ca").asInt() >= 160) && (client.getField("PJ").asInt() <= 765) && (client.getField("Ca").asInt() <= 205)) {
+            i = 0;
+        }
+        int j = client.getField("Wytv").asInt();
+        int k = client.getField("jNPX").asInt();
+        int m = client.getField("Za").asInt();
+        int i1 = client.getField("PJ").asInt();
+        int i3 = client.getField("Ca").asInt();
+
+        if (client.getField("Jmcq").asBoolean()) {
+            int Jmcq = client.getField("Jmcq").asInt();
+            switch (Jmcq) {
+                case 0:
+                    i1 -= 4;
+                    i3 -= 4;
+                    break;
+                case 1:
+                    i1 -= 519;
+                    i3 -= 168;
+                    break;
+                case 2:
+                    i1 -= 5;
+                    i3 -= 338;
+                    break;
+                case 3:
+                    i1 -= 519;
+                    i3 += 0;
+            }
+            int i4 = -1;
+            int i11;
+            int ot = client.getField("ot").asInt();
+
+            for (int i6 = 0; i6 < ot; i6++)
+            {
+                i11 = k + 31 + (ot - 1 - i6) * 15;
+                if ((i1 > j) && (i1 < j + m) && (i3 > i11 - 13) && (i3 < i11 + 3)) {
+                    i4 = i6;
+                }
+            }
+
+            return i4;
+        }
+
+        return -1;
     }
 }
