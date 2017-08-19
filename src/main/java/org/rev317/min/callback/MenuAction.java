fddbd1848e405fb62ad7151480b2e031ee5572f1
JMapfel <sanojakasenoj@web.de>
2017-08-19 22:43:17 +0200

    Revert pkhonor actionRowIndex method

--- src/main/java/org/rev317/min/callback/MenuAction.java
+++ src/main/java/org/rev317/min/callback/MenuAction.java
@@ -1,7 +1,5 @@
 package org.rev317.min.callback;
 
-import org.parabot.core.Context;
-import org.parabot.core.reflect.RefClass;
 import org.rev317.min.Loader;
 import org.rev317.min.accessors.Client;
 import org.rev317.min.api.events.GameActionEvent;
@@ -14,7 +12,7 @@ import org.rev317.min.script.ScriptEngine;
  */
 public class MenuAction {
 
-    private static final String[][] outputs            = {
+    private static final String[][] outputs = {
             {
                     "[index: %d, action1: %d, action2: %d, action3: %d, action4: %d, id: %d]",
                     "[id: %d, action1: %d, action2: %d, action3: %d, action4: %d, index: %d]"
@@ -24,18 +22,17 @@ public class MenuAction {
                     "[id: %d, action1: %d, action2: %d, action3: %d, index: %d]"
             }
     };
-    private static       int        currentOutputIndex = 0;
+    private static int currentOutputIndex = 0;
 
-    public static void intercept() {
-        int index       = getActionIndex();
+    public static void intercept(int index) {
         int outputIndex = 0;
 
-        Client client   = Loader.getClient();
-        int    action1  = client.getMenuAction1()[index];
-        int    action2  = client.getMenuAction2()[index];
-        int    action3  = client.getMenuAction3()[index];
-        int    action4  = 0;
-        int    actionId = client.getMenuActionId()[index];
+        Client client = Loader.getClient();
+        int action1 = client.getMenuAction1()[index];
+        int action2 = client.getMenuAction2()[index];
+        int action3 = client.getMenuAction3()[index];
+        int action4 = 0;
+        int actionId = client.getMenuActionId()[index];
         if (DActions.debugActions()) {
             if (Game.hasAction4()) {
                 action4 = client.getMenuAction4()[index];
@@ -63,57 +60,5 @@ public class MenuAction {
 
     public static String[][] getOutputs() {
         return outputs;
-    }
-
-    private static int getActionIndex() {
-        RefClass client = new RefClass(Context.getInstance().getClient());
-
-        if (client.getField("TiSK").asInt() != 0) {
-            return -1;
-        }
-        int i = client.getField("gDnF").asInt();
-        if ((client.getField("PjW").asInt() == 1) && (client.getField("PJ").asInt() >= 516) && (client.getField("Ca").asInt() >= 160) && (client.getField("PJ").asInt() <= 765) && (client.getField("Ca").asInt() <= 205)) {
-            i = 0;
-        }
-        int j  = client.getField("Wytv").asInt();
-        int k  = client.getField("jNPX").asInt();
-        int m  = client.getField("Za").asInt();
-        int i1 = client.getField("PJ").asInt();
-        int i3 = client.getField("Ca").asInt();
-
-        if (client.getField("Jmcq").asBoolean()) {
-            int Jmcq = client.getField("Jmcq").asInt();
-            switch (Jmcq) {
-                case 0:
-                    i1 -= 4;
-                    i3 -= 4;
-                    break;
-                case 1:
-                    i1 -= 519;
-                    i3 -= 168;
-                    break;
-                case 2:
-                    i1 -= 5;
-                    i3 -= 338;
-                    break;
-                case 3:
-                    i1 -= 519;
-                    i3 += 0;
-            }
-            int i4 = -1;
-            int i11;
-            int ot = client.getField("ot").asInt();
-
-            for (int i6 = 0; i6 < ot; i6++) {
-                i11 = k + 31 + (ot - 1 - i6) * 15;
-                if ((i1 > j) && (i1 < j + m) && (i3 > i11 - 13) && (i3 < i11 + 3)) {
-                    i4 = i6;
-                }
-            }
-
-            return i4;
-        }
-
-        return -1;
     }
 }
